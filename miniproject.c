38abcb3015058737b2a31041bdd4206903a84ad5
hasindunethsara <nethsarahasindu81@gmail.com>
2025-10-25 11:02:22 +0530

    edit display part and voter section

--- miniproject.c
+++ miniproject.c
@@ -91,7 +91,7 @@ void voter(){
 
                 // get voter details
                 printf("Enter your Name: ");
-                scanf("%s", V_name);
+                scanf(" %[^\n]", V_name);
                 printf("Enter your District: ");
                 scanf("%s", V_district);
                 printf("Enter your Password: ");
